79da96c3fc73686faa9a241325c663732cff16d8
rakeshKdigital <jackdaws45@gmail.com>
2023-10-22 14:19:05 +0530

    k

--- routes/post.js
+++ routes/post.js
@@ -210,7 +210,8 @@ const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
 // Configure multer storage for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'assets'); // Specify the directory where uploaded files will be stored
+    const absolutePath = path.resolve(__dirname, 'assets');
+    cb(null, absolutePath); // Specify the directory where uploaded files will be stored
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname); // Use the original file name as the name of the stored file
